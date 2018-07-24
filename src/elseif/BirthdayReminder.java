92c13476e1cd4d3182049530d821b618316082cc
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-07-24 14:27:20 -0700

    Birthday reminder and crazy cat lady

--- src/elseif/BirthdayReminder.java
+++ src/elseif/BirthdayReminder.java
@@ -11,15 +11,22 @@ public class BirthdayReminder {
 	public static void main(String[] args) {
 	
 		// 1. correct the birthdays for your family below
-		String momsBirthday = "January 24th";
-		String dadsBirthday = "June 9th";
-		String myBirthday = "April 16th";
+		String momsBirthday = "September 17th";
+		String dadsBirthday = "June 14th";
+		String myBirthday = "August 10th";
 
 		// 2. Find out which birthday the user wants and and store their response in a variable
-	
+	String input =JOptionPane.showInputDialog("What birthday do you want?");
 		// 3. Print out what the user typed
-		
+	System.out.println(input); 
 		// 4. if user asked for "mom"
+		if (input.equals("mom")) {
+			System.out.println(momsBirthday);
+		}else if (input.equals("dad")) {
+			System.out.println(dadsBirthday);
+		}else if (input.equals("mine")) {
+			System.out.println(myBirthday);
+		}else JOptionPane.showMessageDialog(null,"Sorry, I don't remember that person's birthday!");
 			//print mom's birthday
 		// 5. if user asked for "dad"
 			// print dad's birthday
--- src/elseif/CrazyCatLady.java
+++ src/elseif/CrazyCatLady.java
@@ -5,14 +5,22 @@ package elseif;
 
 import java.net.URI;
 
+import javax.swing.JOptionPane;
+
 public class CrazyCatLady {
 	public static void main(String[] args) {
-		// 1. Ask the user how many cats they have
-
+		// 1. Ask the how many cats they have
+		String input=JOptionPane.showInputDialog(null,"How many cats do you have?");
 		// 2. Convert their answer into an int
-
+			int userAns = Integer.parseInt(input);
 		// 3. If they have 3 or more cats, tell them they are a crazy cat lady
-
+			if (userAns >= 3) {
+				System.out.println("You are a crazy cat lady.");
+			}else if (userAns < 3) {
+			playVideo("https://www.youtube.com/watch?v=dGFSjKuJfrI");
+			}else if (userAns == 0) {
+			playVideo("https://www.youtube.com/watch?v=oj_yLBltPE8");
+			}
 		// 4. If they have less than 3 cats AND more than 0 cats, call the method below to show them a cat video
 
 		// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a Human
